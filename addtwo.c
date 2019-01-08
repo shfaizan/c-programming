ab430b870e98c164defe87a66b77e9f109b0dd38
faizan shaikh <sentry88prime@gmail.com>
2019-01-08 13:09:14 +0530

    Add files via upload

---
+++ addtwo.c
@@ -0,0 +1,14 @@
+//Write a C program to print your name, date of birth. and mobile number
+#include<stdio.h>
+int main()
+{
+    int dob,num;
+    char name;
+    printf("enter name");
+    scanf("%c",name);
+    printf("enter dob and mobile");
+    scanf("%d,%d",dob,num);
+    printf("\n Name = %c",name);
+    return 0;
+}
+
